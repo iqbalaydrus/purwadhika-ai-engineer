d5d9f686bca8d5b5c19d220bf957b6fdc6cb2318
Muhammad Iqbal Alaydrus <iqbal@bangunindo.com>
2025-10-04 14:11:48 +0700

    capstone 3 - create collection

---
+++ capstone-module-3/create-collection.py
@@ -0,0 +1,54 @@
+import os
+
+import pandas as pd
+
+from dotenv import load_dotenv
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+from qdrant_client.http import models as qm
+
+
+load_dotenv()
+QDRANT_COLLECTION_NAME = "cv_resume"
+
+data = pd.read_csv("data/Resume.csv").drop("Resume_html", axis=1)
+docs: list[Document] = []
+for _, row in data.iterrows():
+    metadata = {
+        "id": int(row["ID"]),
+        "category": str(row["Category"]),
+    }
+    docs.append(
+        Document(
+            page_content=str(row["Resume_str"]),
+            metadata=metadata,
+        )
+    )
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    separators=["\n\n", "\n", ". ", " ", ""],
+)
+docs = splitter.split_documents(docs)
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small",
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
+vector_client = QdrantClient(
+    api_key=os.getenv("QDRANT_API_KEY"),
+    url=os.getenv("QDRANT_URL"),
+)
+vector_client.create_collection(
+    collection_name=QDRANT_COLLECTION_NAME,
+    vectors_config=qm.VectorParams(size=1536, distance=qm.Distance.COSINE),
+)
+vector_db = QdrantVectorStore(
+    client=vector_client,
+    embedding=embeddings,
+    collection_name=QDRANT_COLLECTION_NAME,
+)
+vector_db.add_documents(docs)
